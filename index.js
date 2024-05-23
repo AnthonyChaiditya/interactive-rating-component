7081b12700cad59028944327119c93ac71581412
Anthony Chaiditya <achaiditya@gmail.com>
2024-05-23 15:24:07 -0400

    added functionality for switching between components

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // select the rating buttons from the DOM
-
 const ratingButtons = document.querySelectorAll('input');
 
 ratingButtons.forEach((button, index) => {
@@ -15,3 +14,35 @@ function removeActiveClasses() {
     button.classList.remove('active');
   })
 }
+
+// boolean for form submittion to determine conditional rendering
+let submittedForm;
+
+// if submit button is clicked, clear the display for the container
+let endDisplay = document.getElementsByClassName('component-container-end');
+
+function turnThankYouDisplayOn() {
+  return endDisplay[0].attributes[1].value = 'display: flex'
+}
+
+let initialDisplay = document.getElementsByClassName('component-container-start');
+
+function turnInitialDisplayOff() {
+  return initialDisplay[0].attributes[1].value = 'display: none';
+}
+
+console.log(endDisplay[0].attributes)
+
+let submitButton = document.getElementById('submit');
+
+submitButton.addEventListener('click', () => {
+  // take the rating
+  // invoke the thank you display
+  turnInitialDisplayOff();
+  turnThankYouDisplayOn();
+})
+
+
+
+
+

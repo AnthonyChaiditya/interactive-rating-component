4e02f1e33c4683662cc0e629c4f0575f1110a8e7
Anthony Chaiditya <achaiditya@gmail.com>
2024-05-23 14:26:41 -0400

    updated button functionality

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 // select the rating buttons from the DOM
 
-const ratingButtons = document.querySelectorAll('.button');
+const ratingButtons = document.querySelectorAll('input');
 
 ratingButtons.forEach((button, index) => {
   button.addEventListener('click', () => {
     // turn the background color of the button white
+    removeActiveClasses();
     button.classList.add('active');
   })
 })
+
+function removeActiveClasses() {
+  ratingButtons.forEach(button => {
+    button.classList.remove('active');
+  })
+}
